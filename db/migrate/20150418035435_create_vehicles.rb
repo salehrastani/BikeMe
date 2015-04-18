6ebad81b610e4b2ea2d2c54498ae898a3c3996cb
Saleh Rastani <salehrastani@gmail.com>
2015-04-17 21:04:33 -0700

    add migrations for users, drivers, trips, and vehicles

--- db/migrate/20150418035435_create_vehicles.rb
+++ db/migrate/20150418035435_create_vehicles.rb
@@ -1,6 +1,10 @@
 class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
+      t.string :make
+      t.string :model
+      t.string :year
+      t.string :image
 
       t.timestamps null: false
     end
