5118c1e49880e4192f579f1f5489ecc94ff2b241
Saleh Rastani <salehrastani@gmail.com>
2015-04-19 12:01:10 -0700

    change driver model to have many trips, seeded passengers, drivers, and vehicles

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,29 @@
 # require 'faker'
 
 5.times do
-  Passenger.create(name: Faker::Name.name, email: Faker::Internet.email,)
+  Passenger.create(name: Faker::Name.name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
 end
 
-# 5.times do
-#   Driver.create()
-# end
+5.times do
+  Driver.create(name: Faker::Name.name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
+end
 
-# drivers = Driver.all
+5.times do
+  Trip.create(origin: Faker::Address.street_address, destination: Faker::Address.street_address, price: Faker::Commerce.price)
+end
 
-# drivers.each do |driver|
-# driver.vehicles.create(make: "honda", model: "civic", driver_id: driver.id)
-# end
+passengers = Passenger.all
+drivers = Driver.all
+vehicle_model = ["civic", "accord", "jcruiser", "m5", "sonata"]
+vehicle_make = ["honda", "honda", "toyota", "bmw", "hyundai"]
 
+i = 0
+drivers.each do |driver|
+  driver.vehicles.create(make: vehicle_make[i], model: vehicle_model[i], driver_id: driver.id)
+  driver.trips.create()
+  i += 1
+end
+
+passengers.each do |passenger|
+  passenger.trips
+end
