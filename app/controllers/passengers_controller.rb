4303edb3a19eb10b61902c073a3242cebe2377fe
Saleh Rastani <salehrastani@gmail.com>
2015-04-20 11:36:04 -0700

    add form for sign up

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -2,7 +2,20 @@ class PassengersController < ApplicationController
 
   def index
     render :index
+  end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @user_id = session[:user_id]
+    render :dashboard
+  end
+
+  def edit
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'passengers#index'
 
   resources :passengers do
-
   end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
