c22caf7c727d179fd9e06eee75a57d8a8cdec716
Saleh Rastani <salehrastani@gmail.com>
2015-04-21 17:07:15 -0700

    complete new, create method / add sessions ctrlctrl and authorize and current passenger in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def current_passenger
+    @current_passenger ||= User.find(session[:passenger_id]) if session[:passenger_id]
+  end
+
+  def authorize
+    redirect_to '/passengers/new' unless current_passenger
+  end
+
+
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -6,20 +6,42 @@ class PassengersController < ApplicationController
   end
 
   def new
+    @passenger = Passenger.new
+    render :new
   end
 
   def create
-    redirect_to :dashboard
+    @passenger = Passenger.new(passenger_params)
+    if passenger.save
+      session[:passenger_id] = @passenger.id
+      redirect_to "/passengers/#{passenger.id}/dashboard"
+    else
+      redirect_to 'passenger/new'
+    end
+  end
+
+  def login
+    authorize
+  end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to 'passengers/login'
   end
 
   def dashboard
-    render :dashboard
+    render "/passengers/#{passenger.id}/dashboard"
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def passenger_params
+    params.require(:passenger).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
