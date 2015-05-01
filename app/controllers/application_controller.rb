ee2aac2bd70d08c437bdfaa1c434188a47513fcc
Saleh Rastani <salehrastani@gmail.com>
2015-05-01 15:37:02 -0700

    add cors code to accept ajax calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+
   protect_from_forgery with: :exception
 
 
   # def current_passenger
   #   @current_passenger ||= User.find(session[:passenger_id]) if session[:passenger_id]
   # end
+
+
 
 
 end
