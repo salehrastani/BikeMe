b216fa6db0acbc45fc98fccab93e53f2b982b40f
Saleh Rastani <salehrastani@gmail.com>
2015-05-02 15:51:49 -0700

    change drivers controller to redirect to dashboard action once session is made

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -11,23 +11,24 @@ class DriversController < ApplicationController
   end
 
   def create
-    @driver = Driver.new(driver_params)
-    if @driver.save
-      session[:driver_id] = @driver.id
-      render :dashboard
+    driver = Driver.new(driver_params)
+    if driver.save
+      session[:driver_id] = driver.id
+      redirect_to driver_dashboard_path(driver.id)
     else
       redirect_to 'driver/new', :notice => "ewww, please try again"
     end
   end
 
   def login
-    @driver = Driver.find_by_email(params[:email])
+    driver = Driver.find_by_email(params[:passenger][:email])
     # If the user exists AND the password entered is correct.
-    if params[:password] == @driver.password
+    if driver && driver.authenticate(params[:driver][:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
-      session[:driver_id] = @driver.id
-      render :dashboard
+      session[:driver_id] = driver.id
+      redirect_to driver_dashboard_path(driver.id)
+
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/drivers/new', :notice => "Invalid login. Try again"
@@ -44,7 +45,8 @@ class DriversController < ApplicationController
   end
 
   def dashboard
-    render "/drivers/#{@driver.id}/dashboard"
+    @driver = Driver.find(session[:driver_id])
+    render :dashboard
   end
 
   def show
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -11,41 +11,27 @@ class PassengersController < ApplicationController
   end
 
   def create
-    @passenger = Passenger.new(passenger_params)
-    if @passenger.save
-      session[:passenger_id] = @passenger.id
-      redirect_to passenger_dashboard_path(@passenger.id)
+    passenger = Passenger.new(passenger_params)
+    if passenger.save
+      session[:passenger_id] = passenger.id
+      redirect_to passenger_dashboard_path(passenger.id)
     else
       redirect_to 'passenger/new', :notice => "ewww, please try again"
     end
   end
 
   def login
-    @passenger = Passenger.find_by_email(params[:passenger][:email])
+    passenger = Passenger.find_by_email(params[:passenger][:email])
     # If the user exists AND the password entered is correct.
-    if @passenger && @passenger.authenticate(params[:passenger][:password])
+    if passenger && passenger.authenticate(params[:passenger][:password])
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
-      session[:passenger_id] = @passenger.id
-      redirect_to passenger_dashboard_path(@passenger.id)
+      session[:passenger_id] = passenger.id
+      redirect_to passenger_dashboard_path(passenger.id)
     else
     # If user's login doesn't work, send them back to the login form.
       redirect_to '/passengers/new', :notice => "Invalid login. Try again"
     end
-    # p "-------------------------------------------------------------------------"
-    # @passenger = Passenger.find_by_email(params.permit(:email))
-    # p @passenger.email
-    # p @passenger.password
-    # # If the user exists AND the password entered is correct.
-    # if params[:password] == @passenger.password
-    #   # Save the user id inside the browser cookie. This is how we keep the user
-    #   # logged in when they navigate around our website.
-    #   session[:passenger_id] = @passenger.id
-    #   render :dashboard
-    # else
-    # # If user's login doesn't work, send them back to the login form.
-    #   redirect_to '/passengers/new', :notice => "Invalid login. Try again"
-    # end
   end
 
   def authorize
