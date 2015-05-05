b3034270a190066e20d2152add94a190c71d7f30
Saleh Rastani <salehrastani@gmail.com>
2015-05-05 14:42:55 -0700

    include bcrypt code for password digest in driver and passenger model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,4 +6,16 @@ class Driver < ActiveRecord::Base
 
   validates_confirmation_of :password, :message => "no-confirmation"
 
+include BCrypt
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,4 +5,16 @@ class Passenger < ActiveRecord::Base
 
   validates_confirmation_of :password, :message => "no-confirmation"
 
+include BCrypt
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+
 end
