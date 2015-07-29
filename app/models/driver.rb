34d706fa131f12d80a1585a14c886adf5b25779a
Saleh Rastani <salehrastani@gmail.com>
2015-07-28 18:10:07 -0700

    delete extra has_secure_password in driver controller

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -25,25 +25,3 @@ class Driver < ActiveRecord::Base
 
 end
 
-
-  has_secure_password
-  validates_confirmation_of :password, :message => "no-confirmation"
-  after_validation :ensure_token
-  # skip_before_filter  :verify_authenticity_token
-
-  def ensure_token
-    self.token = generate_token
-  end
-
-  def to_json(options={})
-    options[:except] ||= [:password_digest]
-    super(options)
-  end
-
-  private
-    def generate_token
-      loop do
-        token = SecureRandom.uuid
-        break token unless Passenger.where(token:token).first
-      end
-    end
